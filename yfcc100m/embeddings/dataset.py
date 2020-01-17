e7ff07a5b4b56ab8c0fc92d7eb25ad32e8696b00
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-01-17 19:10:26 +0000

    Excluded numbers when writing user tags to file

--- yfcc100m/embeddings/dataset.py
+++ yfcc100m/embeddings/dataset.py
@@ -2,11 +2,14 @@ from yfcc100m.common import load_csv_as_dict
 from yfcc100m.embeddings.autotags import kept_classes
 import pickle
 from tqdm import tqdm
+import re
 
 FIELDS = ["LineNumber", "ID", "Hash", "UserNSID", "UserNickname", "DateTaken", "DateUploaded",
           "CaptureDevice", "Title", "Description", "UserTags", "MachineTags", "Longitude", "Latitude",
           "LongLatAcc", "PageURL", "DownloadURL", "LicenseName", "LicenseURL", "ServerIdentifier",
           "FarmIdentifier", "Secret", "OriginalSecret", "OriginalExtension", "Video"]
+
+USER_TAG_STOP_WORDS = []
 
 
 def count_user_tags(path):
@@ -65,8 +68,8 @@ def images_highest_count_user_tag(path, tag_counts_path=None):
     return highest_counts
 
 
-def dataset_to_file(dataset_path, autotags_path, output_path, tag_freq_thresh=None, tag_counts_path=None,
-                    class_path=None):
+def dataset_to_file(dataset_path, autotags_path, output_path, keep_numbers=None, tag_freq_thresh=None,
+                    tag_counts_path=None, class_path=None):
     """ Reads the dataset and autotags files, and writes the id, user tags, and auto tags
         for each image to the file at output path, by appending the rows to it
 
@@ -78,6 +81,8 @@ def dataset_to_file(dataset_path, autotags_path, output_path, tag_freq_thresh=No
         Path to autotags file
     output_path : str
         File to append rows to
+    keep_numbers : bool
+        Whether to keep numbers, default False
     tag_freq_thresh : int
         User tag frequency threshold for keeping. If not specified all user tags kept
     tag_counts_path : str
@@ -87,6 +92,7 @@ def dataset_to_file(dataset_path, autotags_path, output_path, tag_freq_thresh=No
         Path to classes to keep, to be loaded using kept_classes method. If not specified all classes kept
     """
 
+    keep_numbers = keep_numbers if keep_numbers is not None else False
     dataset = load_csv_as_dict(dataset_path, fieldnames=FIELDS)
     autotags = load_csv_as_dict(autotags_path, fieldnames=["ID", "PredictedConcepts"])
     classes_to_keep = set(kept_classes(class_path)) if class_path else None
@@ -98,15 +104,16 @@ def dataset_to_file(dataset_path, autotags_path, output_path, tag_freq_thresh=No
         autotags_row = next(autotags)
         image_id = dataset_row["ID"]
         image_user_tags = dataset_row["UserTags"]
-        if tag_counts and image_user_tags:
-            image_user_tags = ",".join(
-                [tag for tag in image_user_tags.split(",") if tag_counts[tag] >= tag_freq_thresh])
+        if not keep_numbers or tag_freq_thresh:
+            image_user_tags = ",".join([tag for tag in image_user_tags.split(",")
+                                        if (keep_numbers or not re.match(r"^[0-9]+$", tag))
+                                        and (not tag_counts or tag_counts[tag] >= tag_freq_thresh)])
         if not image_user_tags:
             continue
         image_auto_tags = autotags_row["PredictedConcepts"]
         if classes_to_keep and image_auto_tags:
-            image_auto_tags = [tag_prob for tag_prob in image_auto_tags.split(",") if
-                               tag_prob.split(":")[0] in classes_to_keep]
+            image_auto_tags = ",".join([tag_prob for tag_prob in image_auto_tags.split(",") if
+                                        tag_prob.split(":")[0] in classes_to_keep])
         line = "{}\t{}\t{}\n".format(image_id, image_user_tags, image_auto_tags)
         lines.append(line)
         if len(lines) == 10000000:
