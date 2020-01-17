10f2f503854d9d2a0183b78e7cc17efe14c2fc34
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-01-17 15:49:42 +0000

    Added methods to get classes to keep, and count number of occurrences of classes

--- yfcc100m/embeddings/autotags.py
+++ yfcc100m/embeddings/autotags.py
@@ -26,3 +26,54 @@ def unique_tags(path):
         for concept in predicted_concepts:
             concepts.add(concept)
     return concepts
+
+
+def kept_classes(class_path):
+    """ Get the list of classes to keep
+
+    Parameters
+    ----------
+    class_path : str
+        Path to class file of format "tag,{1 (keep) or 0 (discard)}"
+
+    Returns
+    -------
+    list str
+        List of tags to keep
+    """
+
+    f = open(class_path, "r")
+    lines = f.readlines()
+    classes = []
+    for line in lines:
+        line = line.rstrip()
+        name, kept = line.split(",")
+        if kept == '1':
+            classes.append(name)
+    return classes
+
+
+def class_counts(path):
+    """ Count the number of examples for each class
+
+    Parameters
+    ----------
+    path : str
+        Path to autotags file
+
+    Returns
+    -------
+    dict of str -> int
+        Counts for each class
+    """
+
+    autotags = load_csv_as_dict(path, fieldnames=FIELDS)
+    counts = {}
+    for row in tqdm(autotags):
+        predicted_concepts = [predicted_concept.split(":")[0] for predicted_concept in
+                              row["PredictedConcepts"].split(",")]
+        for concept in predicted_concepts:
+            if not counts.get(concept):
+                counts[concept] = 0
+            counts[concept] += 1
+    return counts
