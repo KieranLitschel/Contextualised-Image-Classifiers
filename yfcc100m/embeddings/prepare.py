97b42d09615559001846e52f2316dab7f58acdea
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-01-20 12:49:36 +0000

    Adjusted methods for preparing dataset so that we don't build our train, validation, and test subsets based on frequencies gathered from the whole dataset (because it's cheating)

--- yfcc100m/embeddings/prepare.py
+++ yfcc100m/embeddings/prepare.py
@@ -2,15 +2,12 @@ from common import load_csv_as_dict, write_rows_to_csv
 from oiv.common import get_train_val_test_flickr_ids
 from yfcc100m.common import get_dataset_fields, get_autotag_fields
 from yfcc100m.autotags import kept_classes
-from yfcc100m.dataset import count_user_tags
 import re
 import os
-import pickle
 from tqdm import tqdm
 
 
-def join_dataset_and_autotags(dataset_path, autotags_path, output_path, keep_numbers=None, tag_freq_thresh=None,
-                              tag_counts_path=None, class_path=None):
+def join_dataset_and_autotags(dataset_path, autotags_path, output_path, keep_numbers=None, class_path=None):
     """ Reads the dataset and autotags files, and writes the id, user tags, and auto tags
         for each image (discarding of videos) to the file at output path, by appending the rows to it
 
@@ -24,11 +21,6 @@ def join_dataset_and_autotags(dataset_path, autotags_path, output_path, keep_num
         File to append rows to
     keep_numbers : bool
         Whether to keep numbers, default False
-    tag_freq_thresh : int
-        User tag frequency threshold for keeping. If not specified all user tags kept
-    tag_counts_path : str
-        Path to pickled dict produced by count_user_tags. If not specified and tag_freq_thresh is not None, calculated
-        by reading the dataset
     class_path : str
         Path to classes to keep, to be loaded using kept_classes method. If not specified all classes kept
     """
@@ -37,9 +29,6 @@ def join_dataset_and_autotags(dataset_path, autotags_path, output_path, keep_num
     dataset = load_csv_as_dict(dataset_path, fieldnames=get_dataset_fields())
     autotags = load_csv_as_dict(autotags_path, fieldnames=get_autotag_fields())
     classes_to_keep = set(kept_classes(class_path)) if class_path else None
-    tag_counts = None
-    if tag_freq_thresh:
-        tag_counts = pickle.load(open(tag_counts_path, "rb")) if tag_counts_path else count_user_tags(dataset_path)
     lines = []
     for dataset_row in tqdm(dataset):
         autotags_row = next(autotags)
@@ -47,10 +36,8 @@ def join_dataset_and_autotags(dataset_path, autotags_path, output_path, keep_num
         image_user_tags = dataset_row["UserTags"]
         if dataset_row["Video"] == "1":
             continue
-        if not keep_numbers or tag_freq_thresh:
-            image_user_tags = ",".join([tag for tag in image_user_tags.split(",")
-                                        if (keep_numbers or not re.match(r"^[0-9]+$", tag))
-                                        and (not tag_counts or tag_counts[tag] >= tag_freq_thresh)])
+        if not keep_numbers:
+            image_user_tags = ",".join([tag for tag in image_user_tags.split(",") if re.match(r"^[0-9]+$", tag)])
         if not image_user_tags:
             continue
         image_auto_tags = autotags_row["PredictedConcepts"]
@@ -90,19 +77,19 @@ def _determine_val_test_ids(path, subset_members):
         Set of flickr id's that belong to validation and test respectively
     """
 
-    thresh_1000 = load_csv_as_dict(path, fieldnames=["ID", "UserTags", "PredictedConcepts"])
-    thresh_1000_ids = []
-    for row in tqdm(thresh_1000):
-        thresh_1000_ids.append(row["ID"])
-    val_test_len = round(len(thresh_1000_ids) * 0.1)
+    raw_dataset = load_csv_as_dict(path, fieldnames=["ID", "UserTags", "PredictedConcepts"])
+    flickr_ids = []
+    for row in tqdm(raw_dataset):
+        flickr_ids.append(row["ID"])
+    val_test_len = round(len(flickr_ids) * 0.1)
     val_ids = set()
     test_ids = set()
-    for flickr_id in thresh_1000_ids:
+    for flickr_id in flickr_ids:
         if flickr_id in subset_members["validation"]:
             val_ids.add(flickr_id)
         elif flickr_id in subset_members["test"]:
             test_ids.add(flickr_id)
-    for flickr_id in thresh_1000_ids:
+    for flickr_id in flickr_ids:
         if flickr_id in subset_members["train"]:
             continue
         elif len(val_ids) < val_test_len:
@@ -114,51 +101,43 @@ def _determine_val_test_ids(path, subset_members):
     return val_ids, test_ids
 
 
-def joined_to_subsets(oiv_folder, thresh_100_path, thresh_1000_path, thresh_100_output_folder,
-                      thresh_1000_output_folder):
-    """ Produces train, validation, and test sets for our dataset with tag_freq_thresh 100 and tag_freq_thresh 1000.
-        Such that the validation and test sets across the two dataset's are each represented by the same flickr images
+def joined_to_subsets(oiv_folder, dataset_path, output_folder):
+    """ Produces train, validation, and test sets for our dataset
 
     Parameters
     ----------
     oiv_folder : str
         Path to folder containing open images csv's
-    thresh_100_path : str
-        Path to file produced by join_dataset_and_autotags with tag_freq_thresh set to 100
-    thresh_1000_path : str
-        Path to file produced by join_dataset_and_autotags with tag_freq_thresh set to 1000
-    thresh_100_output_folder : str
-        Folder to output the train, validation, and test sets to for the dataset with tag_freq_thresh set to 100
-    thresh_1000_output_folder : str
-        Folder to output the train, validation, and test sets to for the dataset with tag_freq_thresh set to 1000
+    dataset_path : str
+        Path to file produced by join_dataset_and_autotags
+    output_folder : str
+        Folder to output the train, validation, and test sets to for the dataset
     """
 
     print("Getting which images are already assigned to subsets in OIV")
     subset_members = get_train_val_test_flickr_ids(oiv_folder)
     print("Determining samples to be used for validation and test sets")
-    val_ids, test_ids = _determine_val_test_ids(thresh_1000_path, subset_members)
+    val_ids, test_ids = _determine_val_test_ids(dataset_path, subset_members)
     subsets = ["train", "validation", "test"]
-    for path, output_folder in [(thresh_100_path, thresh_100_output_folder),
-                                (thresh_1000_path, thresh_1000_output_folder)]:
-        thresh_n = load_csv_as_dict(path, fieldnames=["ID", "UserTags", "PredictedConcepts"])
-        rows_by_subset = {subset: [] for subset in subsets}
-        rows_in_memory = 0
-        print("Dividing samples into subsets")
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-        for row in tqdm(thresh_n):
-            flickr_id = row["ID"]
-            if flickr_id in val_ids:
-                rows_by_subset["validation"].append(row)
-            elif flickr_id in test_ids:
-                rows_by_subset["test"].append(row)
-            elif flickr_id not in subset_members["validation"] and flickr_id not in subset_members["test"]:
-                rows_by_subset["train"].append(row)
-            rows_in_memory += 1
-            if rows_in_memory >= 10000000:
-                for subset in subsets:
-                    write_rows_to_csv(rows_by_subset[subset], os.path.join(output_folder, subset), mode="a")
-                    rows_by_subset[subset] = []
-                rows_in_memory = 0
-        for subset in subsets:
-            write_rows_to_csv(rows_by_subset[subset], os.path.join(output_folder, subset), mode="a")
+    raw_dataset = load_csv_as_dict(dataset_path, fieldnames=["ID", "UserTags", "PredictedConcepts"])
+    rows_by_subset = {subset: [] for subset in subsets}
+    rows_in_memory = 0
+    print("Dividing samples into subsets")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    for row in tqdm(raw_dataset):
+        flickr_id = row["ID"]
+        if flickr_id in val_ids:
+            rows_by_subset["validation"].append(row)
+        elif flickr_id in test_ids:
+            rows_by_subset["test"].append(row)
+        elif flickr_id not in subset_members["validation"] and flickr_id not in subset_members["test"]:
+            rows_by_subset["train"].append(row)
+        rows_in_memory += 1
+        if rows_in_memory >= 10000000:
+            for subset in subsets:
+                write_rows_to_csv(rows_by_subset[subset], os.path.join(output_folder, subset), mode="a")
+                rows_by_subset[subset] = []
+            rows_in_memory = 0
+    for subset in subsets:
+        write_rows_to_csv(rows_by_subset[subset], os.path.join(output_folder, subset), mode="a")
