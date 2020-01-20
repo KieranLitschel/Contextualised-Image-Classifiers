f4bd02b1f4e19033d62d60a5695ab348051fb59c
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-01-20 18:30:40 +0000

    Made building feature encoder a seperate method

--- yfcc100m/embeddings/load.py
+++ yfcc100m/embeddings/load.py
@@ -100,7 +100,33 @@ def count_user_tags(subset_path):
     return counts
 
 
-def load_subset_as_tf_data(path, classes_encoder, features_encoder=None, tag_threshold=None):
+def build_features_encoder(subset_path, tag_threshold=None):
+    """ Takes a set of classes and builds a token text encoder, to convert the str classes to numbers
+
+    Parameters
+    ----------
+    subset_path : str
+        Path to subset to build encoder from
+    tag_threshold : int
+        Threshold over which to keep words as features. If None all are kept
+
+    Returns
+    -------
+    tfds.features.text.TokenTextEncoder
+        Encoder for features
+    """
+
+    tag_threshold = tag_threshold or 1
+    vocab_count = count_user_tags(subset_path)
+    vocab_list = []
+    for vocab, count in vocab_count.items():
+        if tag_threshold >= count:
+            vocab_list.append(vocab)
+    features_encoder = tfds.features.text.TokenTextEncoder(vocab_list, decode_token_separator=",")
+    return features_encoder
+
+
+def load_subset_as_tf_data(path, classes_encoder, features_encoder):
     """ Loads the subset passed, encodes the features, and one hot-encodes the classes
 
     Parameters
@@ -110,37 +136,21 @@ def load_subset_as_tf_data(path, classes_encoder, features_encoder=None, tag_thr
     classes_encoder : tfds.features.text.TokenTextEncoder
         Labels encoder
     features_encoder : tfds.features.text.TokenTextEncoder
-        User tags encoder. If not specified build from scratch
-    tag_threshold : int
-        Used to construct feature extractor if it is not specified. Threshold for keeping tags as features. Default of
-        None. If None all tags are kept
+        User tags encoder
 
     Returns
     -------
     tf.python.data.ops.dataset_ops.DatasetV1Adapter
-        The subset ready for use in TensorFlow, if feature_encoder was not passed then the constructed one is returned
-        as the second element of a tuple
+        The subset ready for use in TensorFlow
     """
 
-    tag_threshold = tag_threshold or 1
     raw_dataset = tf.data.TextLineDataset(path)
     str_features_labels_dataset = raw_dataset.map(
         lambda row: tf.py_function(_process_raw_row, inp=[row], Tout=[tf.string, tf.string]))
-    feature_encoder_was_none = not features_encoder
-    if not features_encoder:
-        print("Building feature encoder")
-        vocab_count = count_user_tags(path)
-        vocab_list = []
-        for vocab, count in vocab_count.items():
-            if tag_threshold >= count:
-                vocab_list.append(vocab)
-        features_encoder = tfds.features.text.TokenTextEncoder(vocab_list, decode_token_separator=",")
     custom_str_row_to_int = partial(_str_row_to_int, features_encoder=features_encoder, classes_encoder=classes_encoder)
     features_labels_dataset = str_features_labels_dataset.map(
         lambda features, labels: tf.py_function(custom_str_row_to_int, inp=[features, labels],
                                                 Tout=[tf.int32, tf.bool]))
-    if feature_encoder_was_none:
-        return features_labels_dataset, features_encoder
     return features_labels_dataset
 
 
@@ -166,8 +176,9 @@ def load_train_val(dataset_folder, classes_set, tag_threshold=None):
     """
 
     classes_encoder = build_classes_encoder(classes_set)
+    features_encoder = build_features_encoder(os.path.join(dataset_folder, "train"), tag_threshold=tag_threshold)
     train_dataset, features_encoder = load_subset_as_tf_data(os.path.join(dataset_folder, "train"), classes_encoder,
-                                                             tag_threshold=tag_threshold)
+                                                             features_encoder=features_encoder)
     val_dataset = load_subset_as_tf_data(os.path.join(dataset_folder, "validation"), classes_encoder,
                                          features_encoder=features_encoder)
     return train_dataset, val_dataset, features_encoder, classes_encoder
