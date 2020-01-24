da02fa0288b596cf3ca7cb9931b8d885f31bdd25
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-01-24 20:33:58 +0000

    Added support for counting detected languages with cld3

--- yfcc100m/dataset.py
+++ yfcc100m/dataset.py
@@ -4,6 +4,7 @@ import pickle
 from tqdm import tqdm
 import pycld2 as cld2
 import re
+import cld3
 
 
 def count_user_tags(path):
@@ -62,8 +63,8 @@ def images_highest_count_user_tag(path, tag_counts_path=None):
     return highest_counts
 
 
-def count_detected_languages(dataset_path, keep_numbers=None):
-    """ Counts detected languages across YFCC100M
+def count_detected_languages_cld2(dataset_path, keep_numbers=None):
+    """ Counts detected languages across YFCC100M using cld2
 
     Parameters
     ----------
@@ -96,4 +97,44 @@ def count_detected_languages(dataset_path, keep_numbers=None):
         if language not in language_counts:
             language_counts[language] = 0
         language_counts[language] += 1
+    return language_counts
+
+
+def count_detected_languages_cld3(dataset_path, keep_numbers=None):
+    """ Counts detected languages across YFCC100M using cld3
+
+    Parameters
+    ----------
+    dataset_path : str
+        Path to dataset file
+    keep_numbers : bool
+        Whether to keep numbers, default False
+
+    Returns
+    -------
+    dict of int -> int
+        Maps language to its detected frequency across YFCC100M
+    """
+
+    keep_numbers = keep_numbers if keep_numbers is not None else False
+    dataset = load_csv_as_dict(dataset_path, fieldnames=get_dataset_fields())
+    language_counts = {}
+    for dataset_row in tqdm(dataset):
+        image_user_tags = dataset_row["UserTags"]
+        if dataset_row["Video"] == "1":
+            continue
+        if not keep_numbers:
+            image_user_tags = ",".join([tag for tag in image_user_tags.split(",") if not re.match(r"^[0-9]+$", tag)])
+        if not image_user_tags:
+            continue
+        image_user_tags = re.sub(r",+", " ", image_user_tags)
+        image_user_tags = re.sub(r"\b(?:https?://|www\.)[a-z0-9-]+(\.[a-z0-9-]+)+(?:[/?].*)?", " ", image_user_tags)
+        lp = cld3.get_language(image_user_tags)
+        lang_code = lp.language
+        is_reliable = lp.is_reliable
+        if not is_reliable:
+            lang_code = "unknown"
+        if lang_code not in language_counts:
+            language_counts[lang_code] = 0
+        language_counts[lang_code] += 1
     return language_counts
