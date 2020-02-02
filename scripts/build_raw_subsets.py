99e6d93ea362a7eb1585386949225ed0dfb318e5
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-02-02 15:37:01 +0000

    Fixed naming

--- scripts/build_raw_subsets.py
+++ scripts/build_raw_subsets.py
@@ -4,7 +4,7 @@ import os
 yfcc_dataset_path = "C:\\Honors Project\\YFCC100M\\yfcc100m_dataset"
 yfcc_autotags_path = "C:\\Honors Project\\YFCC100M\\yfcc100m_autotags"
 output_folder = "C:\\Honors Project\\YFCC100M\\dataset"
-raw_dataset_output_folder = os.path.join(output_folder, "raw_dataset")
+raw_dataset_output = os.path.join(output_folder, "raw_dataset")
 oiv_folder = "C:\\Users\\kiera\\OneDrive - University of Edinburgh\\Every Day Files\\Documents\\Microsoft " \
              "Office\\Word\\Homework\\University\\Year 4\\Honours Project\\Open Images\\v5\\Mixture"
 
@@ -12,6 +12,6 @@ if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 print("Build raw dataset")
-join_dataset_and_autotags(yfcc_dataset_path, yfcc_autotags_path, oiv_folder, raw_dataset_output_folder)
+join_dataset_and_autotags(yfcc_dataset_path, yfcc_autotags_path, oiv_folder, raw_dataset_output,oiv=True)
 print("Build raw subsets")
-joined_to_subsets(oiv_folder, raw_dataset_output_folder, output_folder)
+joined_to_subsets(oiv_folder, raw_dataset_output, output_folder)
