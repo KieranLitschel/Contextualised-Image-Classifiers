cc1f40c25cf8746432a9837601f7e7d51a8aff15
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-02-02 15:14:04 +0000

    Fixed bug

--- scripts/build_raw_subsets.py
+++ scripts/build_raw_subsets.py
@@ -4,14 +4,14 @@ import os
 yfcc_dataset_path = "C:\\Honors Project\\YFCC100M\\yfcc100m_dataset"
 yfcc_autotags_path = "C:\\Honors Project\\YFCC100M\\yfcc100m_autotags"
 output_folder = "C:\\Honors Project\\YFCC100M\\dataset"
-raw_dataset_path = os.path.join(output_folder, "raw_dataset")
+raw_dataset_output_folder = os.path.join(output_folder, "raw_dataset")
 oiv_folder = "C:\\Users\\kiera\\OneDrive - University of Edinburgh\\Every Day Files\\Documents\\Microsoft " \
-             "Office\\Word\\Homework\\University\\Year 4\\Honours Project\\Open Images\\v5\\Bounding Boxes\\Extended"
+             "Office\\Word\\Homework\\University\\Year 4\\Honours Project\\Open Images\\v5\\Mixture"
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 print("Build raw dataset")
-join_dataset_and_autotags(yfcc_dataset_path, yfcc_autotags_path, raw_dataset_path)
+join_dataset_and_autotags(yfcc_dataset_path, yfcc_autotags_path, oiv_folder, raw_dataset_output_folder)
 print("Build raw subsets")
-joined_to_subsets(oiv_folder, raw_dataset_path, output_folder)
+joined_to_subsets(oiv_folder, raw_dataset_output_folder, output_folder)
