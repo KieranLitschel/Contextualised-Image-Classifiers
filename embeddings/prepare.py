41da352ef053b94ec3121fc4c58bc918ede78955
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-02-07 16:36:22 +0000

    Made it so dataset read from YFCC is written to subsets without having to run a separate method

--- embeddings/prepare.py
+++ embeddings/prepare.py
@@ -52,10 +52,38 @@ def pre_process_user_tags(image_user_tags, remove_nums=None, stem=None):
     return image_user_tags
 
 
-def join_dataset_and_autotags(dataset_path, autotags_path, oiv_folder, yfcc_output_path, oiv_output_path,
-                              oiv_human_output_path, stem=None):
+def _write_progress_to_csv(output_folder, yfcc_lines, oiv_lines, oiv_human_verified_lines):
+    """ Write progress building yfcc, oiv human and machine, and oiv human to subset files
+
+    Parameters
+    ----------
+    output_folder : str
+        Folder to output datasets to
+    yfcc_lines : dict of str -> dict of str -> list
+        Lines for YFCC training set
+    oiv_lines : dict of str -> dict of str -> list
+        Lines for OIV training and validation sets with mixtures of machine and human tags
+    oiv_human_verified_lines : dict of str -> dict of str -> list
+        Lines for OIV validation and test sets with only human tags
+    """
+
+    for dataset_folder, lines in zip(["yfcc_lines", "oiv_lines", "oiv_human_verified_lines"],
+                                     [yfcc_lines, oiv_lines, oiv_human_verified_lines]):
+        dataset_folder_path = os.path.join(output_folder, dataset_folder)
+        if not os.path.exists(dataset_folder_path):
+            os.makedirs(dataset_folder_path)
+        for subset in ["train", "validation", "test"]:
+            subset_lines = lines.get(subset)
+            if subset_lines:
+                csv_path = os.path.join(dataset_folder_path, "{}.csv".format(subset))
+                with open(csv_path, "a") as csv:
+                    csv.writelines(subset_lines)
+
+
+def join_dataset_and_autotags(dataset_path, autotags_path, oiv_folder, output_folder, stem=None):
     """ Reads the dataset and autotags files, and writes the id, user tags (stemmed if stemmable language detected), and
-        auto tags for each image (discarding of videos) to the file at output path, by appending the rows to it
+        auto tags for each image (discarding of videos) to the datasets subsets at the output path, by appending the
+        rows to it. Does so for datasets YFCC, OIV human and machine, and OIV human
 
     Parameters
     ----------
@@ -65,12 +93,8 @@ def join_dataset_and_autotags(dataset_path, autotags_path, oiv_folder, yfcc_outp
         Path to autotags file
     oiv_folder : str
         Path to folder of Image ID files of Open Images for train, validation, and test
-    yfcc_output_path : str
-        File to append yfcc rows to that do not appear in oiv validation and test
-    oiv_output_path : str
-        File to append oiv rows to
-    oiv_human_output_path : str
-        File to append oiv rows with human verified labels to
+    output_folder : str
+        Folder to output datasets to
     stem : bool
         Whether to apply stemming, default is True
     """
@@ -87,9 +111,10 @@ def join_dataset_and_autotags(dataset_path, autotags_path, oiv_folder, yfcc_outp
     oiv_image_labels = get_labels_detected_in_images(oiv_folder, classes_to_keep=classes_to_keep,
                                                      get_confidence=True)
     yfcc100m_oiv_labels_map = get_yfcc100m_oiv_labels_map()
-    yfcc_lines = []
-    oiv_lines = []
-    oiv_human_verified_lines = []
+    yfcc_lines = {"train": []}
+    oiv_lines = {"train": [], "validation": []}
+    oiv_human_verified_lines = {"validation": [], "test": []}
+    lines_in_memory = 0
     print("Building dataset")
     for dataset_row in tqdm(dataset):
         autotags_row = next(autotags)
@@ -131,8 +156,15 @@ def join_dataset_and_autotags(dataset_path, autotags_path, oiv_folder, yfcc_outp
             image_labels = [(tag, confidence) for tag, confidence in image_labels if tag in classes_to_keep]
         image_labels_str = ",".join("{}:{}".format(tag, confidence) for tag, confidence in image_labels)
         line = "{}\t{}\t{}\n".format(image_id, image_user_tags, image_labels_str)
-        if image_id in oiv_image_ids["validation"] or image_id in oiv_image_ids["test"]:
-            oiv_lines.append(line)
+        subset = "train"
+        if image_id in oiv_image_ids["validation"]:
+            subset = "validation"
+        elif image_id in oiv_image_ids["test"]:
+            subset = "test"
+        if subset == "validation":
+            oiv_lines[subset].append(line)
+            lines_in_memory += 1
+        if subset == "validation" or subset == "test":
             human_image_labels = []
             for tag, confidence in image_labels:
                 if confidence == 0 or confidence == 1:
@@ -140,113 +172,22 @@ def join_dataset_and_autotags(dataset_path, autotags_path, oiv_folder, yfcc_outp
             if human_image_labels:
                 human_image_labels_str = ",".join("{}:{}".format(tag, confidence)
                                                   for tag, confidence in human_image_labels)
-                oiv_human_verified_lines.append(human_image_labels_str)
+                oiv_human_verified_lines[subset].append(human_image_labels_str)
+                lines_in_memory += 1
         elif image_id in oiv_image_ids["train"]:
-            oiv_lines.append(line)
-            yfcc_lines.append(line)
+            oiv_lines[subset].append(line)
+            yfcc_lines[subset].append(line)
+            lines_in_memory += 2
         else:
-            yfcc_lines.append(line)
-        if len(yfcc_lines) == 10000000:
-            output_file = open(yfcc_output_path, "a")
-            output_file.writelines(yfcc_lines)
-            output_file.close()
-            yfcc_lines = []
-    if yfcc_lines:
-        yfcc_lines[-1] = yfcc_lines[-1][:-1]
-        output_file = open(yfcc_output_path, "a")
-        output_file.writelines(yfcc_lines)
-        output_file.close()
-    output_file = open(oiv_output_path, "a")
-    output_file.writelines(oiv_lines)
-    output_file.close()
-    output_file = open(oiv_human_output_path, "a")
-    output_file.writelines(oiv_human_verified_lines)
-    output_file.close()
-
-
-def _determine_val_test_ids(path, subset_members):
-    """ Determine which flickr id's should belong to the validation and test sets respectively. Such that each is
-        a sample of 10% of the number of samples available, and that allocations to train, validation, and test sets
-        by OIV are respected
-
-    Parameters
-    ----------
-    path : str
-        Path to file produced by join_dataset_and_autotags
-    subset_members : dict of str -> set
-        Dict mapping "train", "validation", and "test" to sets, containing the flickr ids of images that belong to them.
-        Produced by oiv.common.get_train_val_test_flickr_ids
-
-    Returns
-    -------
-    (set of str, set of str)
-        Set of flickr id's that belong to validation and test respectively
-    """
-
-    raw_dataset = load_csv_as_dict(path, fieldnames=["ID", "UserTags", "PredictedConcepts"])
-    flickr_ids = []
-    for row in tqdm(raw_dataset):
-        flickr_ids.append(row["ID"])
-    val_test_len = round(len(flickr_ids) * 0.1)
-    val_ids = set()
-    test_ids = set()
-    for flickr_id in flickr_ids:
-        if flickr_id in subset_members["validation"]:
-            val_ids.add(flickr_id)
-        elif flickr_id in subset_members["test"]:
-            test_ids.add(flickr_id)
-    for flickr_id in flickr_ids:
-        if flickr_id in subset_members["train"]:
-            continue
-        elif len(val_ids) < val_test_len:
-            val_ids.add(flickr_id)
-        elif len(test_ids) < val_test_len:
-            test_ids.add(flickr_id)
-        else:
-            break
-    return val_ids, test_ids
-
-
-def joined_to_subsets(oiv_folder, dataset_path, output_folder):
-    """ Produces train, validation, and test sets for our dataset
-
-    Parameters
-    ----------
-    oiv_folder : str
-        Path to folder containing open images csv's
-    dataset_path : str
-        Path to file produced by join_dataset_and_autotags
-    output_folder : str
-        Folder to output the train, validation, and test sets to for the dataset
-    """
-
-    print("Getting which images are already assigned to subsets in OIV")
-    subset_members = get_train_val_test_ids(oiv_folder)
-    print("Determining samples to be used for validation and test sets")
-    val_ids, test_ids = _determine_val_test_ids(dataset_path, subset_members)
-    subsets = ["train", "validation", "test"]
-    raw_dataset = load_csv_as_dict(dataset_path, fieldnames=["ID", "UserTags", "PredictedConcepts"])
-    rows_by_subset = {subset: [] for subset in subsets}
-    rows_in_memory = 0
-    print("Dividing samples into subsets")
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    for row in tqdm(raw_dataset):
-        flickr_id = row["ID"]
-        if flickr_id in val_ids:
-            rows_by_subset["validation"].append(row)
-        elif flickr_id in test_ids:
-            rows_by_subset["test"].append(row)
-        elif flickr_id not in subset_members["validation"] and flickr_id not in subset_members["test"]:
-            rows_by_subset["train"].append(row)
-        rows_in_memory += 1
-        if rows_in_memory >= 10000000:
-            for subset in subsets:
-                write_rows_to_csv(rows_by_subset[subset], os.path.join(output_folder, subset), mode="a")
-                rows_by_subset[subset] = []
-            rows_in_memory = 0
-    for subset in subsets:
-        write_rows_to_csv(rows_by_subset[subset], os.path.join(output_folder, subset), mode="a")
+            yfcc_lines[subset].append(line)
+            lines_in_memory += 1
+        if lines_in_memory == 10000000:
+            _write_progress_to_csv(output_folder, yfcc_lines, oiv_lines, oiv_human_verified_lines)
+            yfcc_lines = {"train": []}
+            oiv_lines = {"train": [], "validation": []}
+            oiv_human_verified_lines = {"validation": [], "test": []}
+            lines_in_memory = 0
+    _write_progress_to_csv(output_folder, yfcc_lines, oiv_lines, oiv_human_verified_lines)
 
 
 def build_dataset(dataset_dir, classes_encoder_path, output_folder, tag_threshold, user_tags_limit):
--- scripts/build_raw_datasets.py
+++ scripts/build_raw_datasets.py
@@ -1,4 +1,4 @@
-from embeddings.prepare import join_dataset_and_autotags, joined_to_subsets
+from embeddings.prepare import join_dataset_and_autotags
 import os
 
 yfcc_dataset_path = "C:\\Honors Project\\YFCC100M\\yfcc100m_dataset"
@@ -6,14 +6,10 @@ yfcc_autotags_path = "C:\\Honors Project\\YFCC100M\\yfcc100m_autotags"
 output_folder = "C:\\Honors Project\\YFCC100M\\dataset"
 oiv_folder = "C:\\Users\\kiera\\OneDrive - University of Edinburgh\\Every Day Files\\Documents\\Microsoft " \
              "Office\\Word\\Homework\\University\\Year 4\\Honours Project\\Open Images\\v5\\Mixture"
-raw_dataset_yfcc_output = os.path.join(output_folder, "raw_dataset_yfcc")
-raw_dataset_oiv_human_machine_output = os.path.join(output_folder, "raw_dataset_oiv_human_machine")
-raw_dataset_oiv_human_output = os.path.join(output_folder, "raw_dataset_oiv_human")
 stem = False
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 print("Build raw dataset")
-join_dataset_and_autotags(yfcc_dataset_path, yfcc_autotags_path, oiv_folder, raw_dataset_yfcc_output,
-                          raw_dataset_oiv_human_machine_output, raw_dataset_oiv_human_output, stem=stem)
+join_dataset_and_autotags(yfcc_dataset_path, yfcc_autotags_path, oiv_folder, output_folder, stem=stem)
