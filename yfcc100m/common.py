fb3f0c01c66fa09dec698143dd9a951324238acf
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-02-13 18:44:05 +0000

    Added method to merge results produced by OIV.extend into YFCC100M

--- yfcc100m/common.py
+++ yfcc100m/common.py
@@ -1,4 +1,6 @@
 import re
+import os
+from common import load_csv_as_dict, write_rows_to_csv
 
 
 def extract_image_id_from_flickr_static(static_url):
@@ -50,3 +52,58 @@ def get_autotag_fields():
     """
 
     return ["ID", "PredictedConcepts"]
+
+
+def get_places_fields():
+    """ Return the fields for places file
+
+    Returns
+    -------
+    list of str
+        Fields for places file
+    """
+
+    return ["ID", "Places"]
+
+
+def extend_dataset(oiv_dir, validation_extend_path, test_extend_path):
+    """ Extends the OIV dataset using the validation and test expansions produced by oiv.extend. Appends to
+        yfcc100m_dataset, but if yfcc100m_places and/or yfcc100m_autotags is present in the folder too, also adds
+        empty rows to them so that the lines across all files correspond to the same images
+
+    Parameters
+    ----------
+    oiv_dir : str
+        Directory to OIV files to extends
+    validation_extend_path : str
+        Directory to validation user tags extension file produced by oiv.extend
+    test_extend_path : str
+        Directory to test user tags extension file produced by oiv.extend
+
+    """
+
+    dataset_rows = []
+    autotags_rows = []
+    places_rows = []
+    for path in [validation_extend_path, test_extend_path]:
+        extend_csv = load_csv_as_dict(path, delimiter="\t", fieldnames=["flickr_id", "user_tags"])
+        for row in extend_csv:
+            dataset_row = {field: "" for field in get_dataset_fields()}
+            autotags_row = {field: "" for field in get_autotag_fields()}
+            places_row = {field: "" for field in get_places_fields()}
+            for yfcc_row in [dataset_row, autotags_row, places_row]:
+                yfcc_row["ID"] = row["flickr_id"]
+            dataset_row["UserTags"] = row["user_tags"]
+            dataset_row["Video"] = "0"
+            dataset_rows.append(dataset_row)
+            autotags_rows.append(autotags_row)
+            places_rows.append(places_rows)
+    dataset_path = os.path.join(oiv_dir, "yfcc100m_dataset")
+    if os.path.exists(dataset_path):
+        write_rows_to_csv(dataset_rows, dataset_path, fieldnames=get_dataset_fields(), mode="a", delimiter="\t")
+    autotags_path = os.path.join(oiv_dir, "yfcc100m_autotags")
+    if os.path.exists(autotags_path):
+        write_rows_to_csv(autotags_rows, autotags_path, fieldnames=get_autotag_fields(), mode="a", delimiter="\t")
+    places_path = os.path.join(oiv_dir, "yfcc100m_places")
+    if os.path.exists(places_path):
+        write_rows_to_csv(places_rows, places_path, fieldnames=get_places_fields(), mode="a", delimiter="\t")
