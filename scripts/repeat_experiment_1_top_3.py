8e5175747bc36e8f4e627bfaa2681c7ee1b59c91
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-03-25 16:05:00 +0000

    Added script to take the top 3 best results in experiment_1 for each combination of pad_size and tag_threshold, and repeat them a further two times with different random seeds

---
+++ scripts/repeat_experiment_1_top_3.py
@@ -0,0 +1,55 @@
+""" Takes the top 3 best results in experiment_1 for each combination of pad_size and tag_threshold, and repeat them
+a further two times with different random seeds """
+
+from common import load_csv_as_dict
+import re
+import pathlib
+import os
+
+proj_root = pathlib.Path(__file__).parent.parent.absolute()
+
+results = list(load_csv_as_dict(os.path.join(proj_root, "results/experiment_1_summary.csv"), delimiter=",",
+                                fieldnames=["experiment_no", "experiment_name", "config", "map"]))
+unique_results = {}
+for result in results:
+    if not unique_results.get(result["config"]):
+        unique_results[result["config"]] = result["map"]
+
+pad_sizes = set()
+tag_threshs = set()
+
+results_by_lang_hyperparams = {}
+for config, mAP in unique_results.items():
+    pad_size = int(re.findall(r"--pad_size=([0-9]+)", config)[0])
+    pad_sizes.add(pad_size)
+    tag_thresh = int(re.findall(r"--tag_threshold=([0-9]+)", config)[0])
+    tag_threshs.add(tag_thresh)
+    pooling = re.findall(r"--pooling_layer=([^\s]+)", config)[0]
+    if not results_by_lang_hyperparams.get(pad_size):
+        results_by_lang_hyperparams[pad_size] = {}
+    if not results_by_lang_hyperparams[pad_size].get(tag_thresh):
+        results_by_lang_hyperparams[pad_size][tag_thresh] = {}
+    mAP = float(mAP)
+    results_by_lang_hyperparams[pad_size][tag_thresh][config] = mAP
+
+job_script = open(
+    os.path.join(proj_root, "milano_configs/embeddings_experiment_run.sh"),
+    "r").read()
+
+for escaped_symbol in ["$", '"', "`"]:
+    job_script = re.sub(r"\\\{}".format(escaped_symbol), escaped_symbol, job_script)
+
+for pad_size in pad_sizes:
+    for tag_thresh in tag_threshs:
+        top_three = sorted(list(results_by_lang_hyperparams[pad_size][tag_thresh].items()), key=lambda x: x[1],
+                           reverse=True)[:3]
+        job_no = 0
+        for config, _ in top_three:
+            for seed in range(1, 3):
+                new_script = job_script.replace("--random_seed 0", "--random_seed {}".format(seed)) \
+                    .replace('"$@"', config).replace("${SLURM_JOB_NAME%???}",
+                                                     "repeat_{}_{}_{}_{}".format(pad_size, tag_thresh, job_no, seed))
+                with open("run.sh", "w") as f:
+                    f.write(new_script)
+                os.system("sbatch run.sh")
+            job_no += 1
