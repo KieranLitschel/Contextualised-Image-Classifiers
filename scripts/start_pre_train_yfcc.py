08e25bc47f4bb7494dca491386cbccf47c210451
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-04-09 19:51:38 +0100

    Fixed bug

--- scripts/start_pre_train_yfcc.py
+++ scripts/start_pre_train_yfcc.py
@@ -8,7 +8,7 @@ proj_root = pathlib.Path(__file__).parent.parent.absolute()
 pre_train_path = os.path.join(proj_root, "milano_configs/embeddings_pretrain_run.sh")
 
 for i in range(0, 3):
-    script = re.sub('export RANDOM_SEED="0"', "\n".join(open(pre_train_path, "r").readlines()),
+    script = re.sub('export RANDOM_SEED="0"', "".join(open(pre_train_path, "r").readlines()),
                     'export RANDOM_SEED="{}"'.format(i))
     output_script = "pre_train_{}.sh".format(i)
     open(output_script, "w").write(script)
