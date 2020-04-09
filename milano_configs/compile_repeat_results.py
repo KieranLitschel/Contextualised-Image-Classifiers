d93b33468b059624b08a73ac1f41c8e4d3fa59b6
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-04-09 12:38:18 +0100

    Made it so doesn't crash if files missing

--- milano_configs/compile_repeat_results.py
+++ milano_configs/compile_repeat_results.py
@@ -14,20 +14,25 @@ folders = os.listdir(experiment_path)
 
 for folder_name in tqdm(folders):
     job_output_folder = os.path.join(experiment_path, folder_name)
-    job_results_file = os.path.join(job_output_folder, "best_model_validation_metrics.csv")
-    with open(os.path.join(job_output_folder, "script_args.pickle"), "rb") as script_args_file:
-        script_args = pickle.load(script_args_file)
-    mean_average_precision = \
-        re.findall(r"mAP@0\.5IOU,([0-9]+\.[0-9]+)", open(job_results_file, "r").readlines()[1])[0]
-    hyper_params = "--pad_size={} --tag_threshold={} --layer_capacity={} --pooling_layer={} --batch_size={} " \
-                   "--learning_rate={} --dropout_rate={} --random_seed={}".format(script_args.pad_size,
-                                                                                  script_args.tag_threshold,
-                                                                                  script_args.layer_capacity,
-                                                                                  script_args.pooling_layer,
-                                                                                  script_args.batch_size,
-                                                                                  script_args.learning_rate,
-                                                                                  script_args.dropout_rate,
-                                                                                  script_args.random_seed)
+    job_results_path = os.path.join(job_output_folder, "best_model_validation_metrics.csv")
+    script_args_path = os.path.join(job_output_folder, "script_args.pickle")
+    mean_average_precision = ""
+    hyper_params = ""
+    if os.path.exists(job_results_path):
+        mean_average_precision = \
+            re.findall(r"mAP@0\.5IOU,([0-9]+\.[0-9]+)", open(job_results_path, "r").readlines()[1])[0]
+    if os.path.exists(script_args_path):
+        with open(script_args_path, "rb") as script_args_file:
+            script_args = pickle.load(script_args_file)
+        hyper_params = "--pad_size={} --tag_threshold={} --layer_capacity={} --pooling_layer={} --batch_size={} " \
+                       "--learning_rate={} --dropout_rate={} --random_seed={}".format(script_args.pad_size,
+                                                                                      script_args.tag_threshold,
+                                                                                      script_args.layer_capacity,
+                                                                                      script_args.pooling_layer,
+                                                                                      script_args.batch_size,
+                                                                                      script_args.learning_rate,
+                                                                                      script_args.dropout_rate,
+                                                                                      script_args.random_seed)
     result = {"JobName": folder_name, "HyperParams": hyper_params, "mAP": mean_average_precision}
     write_rows_to_csv([result], results_csv, mode="a", delimiter=",",
                       fieldnames=["JobName", "HyperParams", "mAP"])
