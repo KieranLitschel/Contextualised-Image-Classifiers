7a9272211fafc8159584b3ea5cd84a7b36885bdc
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-04-09 17:49:50 +0100

    Added script to compute mean and std for results from experiment 1

---
+++ scripts/experiment_1_compute_mean_and_std.py
@@ -0,0 +1,31 @@
+""" Computes the mean and standard deviation for the results from the repeats of experiment 1 """
+
+import pathlib
+import os
+import numpy as np
+
+from common import load_csv_as_dict, write_rows_to_csv
+
+proj_root = pathlib.Path(__file__).parent.parent.absolute()
+
+experiment_1_merged_path = os.path.join(proj_root, "results/experiment_1_repeat_merged_summary.csv")
+experiment_1_mean_and_std = os.path.join(proj_root, "results/experiment_1_with_mean_and_std.csv")
+
+job_name = None
+hyper_params = None
+mAPs = []
+
+summaries = []
+
+for row in load_csv_as_dict(experiment_1_merged_path, fieldnames=["JobName", "HyperParams", "mAP"], delimiter=","):
+    curr_hyper_parms = " ".join(row["HyperParams"].split(" ")[:-1])
+    if hyper_params is not None and curr_hyper_parms != hyper_params:
+        summaries.append(
+            {"JobName": job_name, "HyperParams": hyper_params, "mean mAP": np.mean(mAPs), "std mAP": np.std(mAPs)})
+        mAPs = []
+    job_name = "_".join(row["JobName"].split("_")[:-1])
+    hyper_params = curr_hyper_parms
+    mAPs.append(float(row["mAP"]))
+
+write_rows_to_csv(summaries, experiment_1_mean_and_std, fieldnames=["JobName", "HyperParams", "mean mAP", "std mAP"],
+                  mode="w", delimiter=",")
